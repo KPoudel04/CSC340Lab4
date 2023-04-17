9d8e7f1ae5263f82fa0a015ecca051d8b0cbc034
memo <grreyes>
2023-04-16 21:14:45 -0700

    Moving to sentenceSplitter

--- fileIOs_wordPairs.cpp
+++ fileIOs_wordPairs.cpp
@@ -83,30 +83,30 @@ namespace F_WORDSPAIRS
             string phrase = "";
             int startI = 0;
 
-            // loop through every character of the input fname
-            while (startI < fname.length())
+            // loop through every character of the input line
+            while (startI < line.length())
             {
                 // find the position of the first occurrence of a period, question mark, or newline character
-                size_t newline_pos = fname.find_first_of(".?\n", startI);
+                size_t newline_pos = line.find_first_of(".?\n", startI);
 
-                // if the end of the fname has been reached, set the newline position to the end of the fname
+                // if the end of the line has been reached, set the newline position to the end of the line
                 if (newline_pos == string::npos)
                 {
-                    newline_pos = fname.length();
+                    newline_pos = line.length();
                 }
 
                 // check if the next character after the newline is a double quotation mark
-                if ((newline_pos + 1) < fname.length() && fname.at(newline_pos + 1) == '\"')
+                if ((newline_pos + 1) < line.length() && line.at(newline_pos + 1) == '\"')
                 {
                     // if it is, append the current phrase and the next character (double quotation mark) to the sentences vector
-                    phrase = fname.substr(startI, newline_pos + 1 - startI);
+                    phrase = line.substr(startI, newline_pos + 1 - startI);
                     sentences.push_back(phrase);
                     startI = newline_pos + 2; // set the starting index of the next sentence to the character after the double quotation mark
                     continue;
                 }
 
                 // if the next character after the newline is not a double quotation mark, append the current phrase to the sentences vector
-                phrase = fname.substr(startI, newline_pos - startI);
+                phrase = line.substr(startI, newline_pos - startI);
                 sentences.push_back(phrase);
                 startI = newline_pos + 1; // set the starting index of the next sentence to the character after the period or question mark
                  
