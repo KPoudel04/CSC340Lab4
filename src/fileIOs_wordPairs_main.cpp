90c77710ff515269d2009c02b8e6e4c673f57c88
Kinzokuzu <sauljcervantes2@gmail.com>
2023-04-19 08:47:04 -0700

    Look at TO-DOs on main

--- src/fileIOs_wordPairs_main.cpp
+++ src/fileIOs_wordPairs_main.cpp
@@ -1,6 +1,13 @@
 #include "fileIOs_wordPairs.h"
 
 int main() {
+  // TO-DO: Open and store sentences from file into a vector of strings
+
+  // TO-DO: Map wordpairs and their frequency
+
+  // TO-DO: Flip keys/values from wordpair map
+
+  // TO-DO: Create and write the most and least frequent wordpairs to a file
 
   return 0;
 }
